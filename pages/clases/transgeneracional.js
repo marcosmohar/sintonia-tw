ffb5e19fe9e5d7df9af962ccff810c4e3aa0b86f
marcosmohar <elcabeca@gmail.com>
2022-02-08 09:25:40 -0600

    actualizaciones iconos y titulos

--- pages/clases/transgeneracional.js
+++ pages/clases/transgeneracional.js
@@ -356,7 +356,7 @@ export default function Transgeneracional() {
               />
               <div>
                 <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-sintogreen-700 uppercase rounded-full bg-sintogreen-50">
-                  Curso
+                  Curso nuevo
                 </p>
               </div>
               <h2 className="max-w-lg mb-4 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
@@ -408,23 +408,23 @@ export default function Transgeneracional() {
             </div>
           </div>
         </div>
-        <div className="relative px-4 sm:px-0 ">
-          <div className="absolute inset-0 bg-gray-100 h-1/2" />
+        <div className="relative px-4 sm:px-0 pt-10">
+          <div className="absolute inset-0 bg-sintopurple-600 h-32" />
           <div className="relative grid mx-auto overflow-hidden bg-white divide-y rounded shadow sm:divide-y-0 sm:divide-x sm:max-w-screen-sm sm:grid-cols-3 lg:max-w-screen-md">
             <div className="inline-block p-8 text-center">
               <div className="flex items-center justify-center w-12 h-12 mx-auto mb-4 rounded-full bg-indigo-50">
                 <svg
-                  className="w-10 h-10 text-deep-purple-accent-400"
+                  className="w-6 h-6 text-sintopurple-700"
                   stroke="currentColor"
-                  viewBox="0 0 52 52"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
-                  <polygon
-                    strokeWidth="3"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    fill="none"
-                    points="29 13 14 29 25 29 23 39 38 23 27 23"
-                  />
+                  <line x1="12" y1="1" x2="12" y2="23"></line>
+                  <path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>
                 </svg>
               </div>
               <p className="font-bold tracking-wide text-gray-800">$2,200</p>
@@ -453,17 +453,18 @@ export default function Transgeneracional() {
             <div className="inline-block p-8 text-center">
               <div className="flex items-center justify-center w-12 h-12 mx-auto mb-4 rounded-full bg-indigo-50">
                 <svg
-                  className="w-10 h-10 text-deep-purple-accent-400"
+                  className="w-6 h-6 text-sintopurple-700"
+                  viewBox="0 0 24 24"
+                  fill="none"
                   stroke="currentColor"
-                  viewBox="0 0 52 52"
+                  strokeWidth="2.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
-                  <polygon
-                    strokeWidth="3"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    fill="none"
-                    points="29 13 14 29 25 29 23 39 38 23 27 23"
-                  />
+                  <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+                  <line x1="16" y1="2" x2="16" y2="6"></line>
+                  <line x1="8" y1="2" x2="8" y2="6"></line>
+                  <line x1="3" y1="10" x2="21" y2="10"></line>
                 </svg>
               </div>
               <p className="font-bold tracking-wide text-gray-800">
@@ -474,7 +475,7 @@ export default function Transgeneracional() {
         </div>
       </div>
       <section className="max-w-2xl mx-auto mb-10 px-2">
-        <h2 className=" mb-10 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900  md:mx-auto">
+        <h2 className=" mb-10 font-sans text-2xl lg:text-3xl font-bold leading-none tracking-tight text-gray-900  md:mx-auto">
           Qué es Transgeneracional?
         </h2>
         <p className="mb-4 text-base">
@@ -498,7 +499,7 @@ export default function Transgeneracional() {
       </section>
       <section className="max-w-2xl mx-auto mb-20 px-2 pb-10">
         <div className="mb-10">
-          <h2 className=" mb-4 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900  md:mx-auto">
+          <h2 className=" mb-4 font-sans text-2xl lg:text-3xl font-bold leading-none tracking-tight text-gray-900  md:mx-auto">
             ¿Qué necesito para tomar la clase?
           </h2>
           <p className="text-base mb-4">
@@ -506,7 +507,7 @@ export default function Transgeneracional() {
           </p>
         </div>
         <div className="mb-10">
-          <h2 className=" mb-4 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900  md:mx-auto">
+          <h2 className=" mb-4 font-sans text-2xl lg:text-3xl font-bold leading-none tracking-tight text-gray-900  md:mx-auto">
             A quién va dirigida esta clase
           </h2>
           <p className="text-base mb-4">
